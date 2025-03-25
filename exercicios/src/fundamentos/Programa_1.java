971228a07d3a327be816b47460ac9aa1b1446015
Felipe Rezende <felipera776@gmail.com>
2025-03-25 16:07:22 -0300

    2 adição

--- exercicios/src/fundamentos/Programa_1.java
+++ exercicios/src/fundamentos/Programa_1.java
@@ -1,5 +1,20 @@
 package fundamentos;
 
+/**
+ * Esse /* é para teste ele serve para documentar a class
+ * 
+ * @author felip
+ * @since JDK1.0
+ */
+
 public class Programa_1 {
+	public static void main(String[] args) {
+		// Sempre importante deixar um comentario para deixar gravado oque a sentença
+		System.out.println("aoopaaa");
+		System.out.println("tudo bem ?");
+		System.out.println("Estou bem sim e você");
+		System.out.println("Estou bem tambêm");
+
+	}
 
 }
